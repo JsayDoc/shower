15331a4df875efc52a03d8e0be2c33ffe3a0777e
Arnak <arnak@melikyan.am>
2021-12-10 11:29:28 +0400

    Added new API's

--- api_data.js
+++ api_data.js
@@ -1,6 +1,243 @@
 define({ "api": [
   {
     "type": "post",
+    "url": "accounts/auth/",
+    "title": "Авторизация через почту",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    \"token\": {\n        \"refresh\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6M\",\n        \"access\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNT\"\n}\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Информация о токене пользователя</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.refresh",
+            "description": "<p>Refresh токен для продления основного токена</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.access",
+            "description": "<p>Токен пользователя</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "1.Error-Response:",
+          "content": "{\n    \"error_code\": 1,\n    \"error_message\": \"Неверный емейл или пароль\"\n}",
+          "type": "json"
+        },
+        {
+          "title": "2.Error-Response:",
+          "content": "{\n    \"error_code\": 2,\n    \"error_message\": \"30\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Error Code": [
+          {
+            "group": "Error Code",
+            "optional": false,
+            "field": "1",
+            "description": "<p>Неверный емейл или пароль</p>"
+          },
+          {
+            "group": "Error Code",
+            "optional": false,
+            "field": "2",
+            "description": "<p>Время блокировки в секундах</p>"
+          }
+        ]
+      }
+    },
+    "version": "1.0.0",
+    "name": "auth",
+    "group": "Аутентификация",
+    "description": "<p>Авторизация через почту (форма логина)</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Валидный e-mail - адрес почты</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Пароль пользователя, минимум <code>6</code> символов (минимум <code>1</code> цифра и <code>1</code> буква)</p>"
+          }
+        ]
+      }
+    },
+    "filename": "shower/apps/account/rest/v1/api.py",
+    "groupTitle": "Аутентификация"
+  },
+  {
+    "type": "post",
+    "url": "accounts/registration/",
+    "title": "Регистрация через почту",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    \"token\": {\n        \"refresh\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6M\",\n        \"access\": \"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNT\"\n    }\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "Object",
+            "optional": false,
+            "field": "token",
+            "description": "<p>Информация о токене пользователя</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.refresh",
+            "description": "<p>Refresh токен для продления основного токена</p>"
+          },
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "token.access",
+            "description": "<p>Токен пользователя</p>"
+          }
+        ]
+      }
+    },
+    "version": "1.0.0",
+    "name": "registration",
+    "group": "Аутентификация",
+    "description": "<p>Регистрация через почту (форма регистрации)</p>",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "email",
+            "description": "<p>Валидный e-mail - адрес почты</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "password",
+            "description": "<p>Пароль пользователя, минимум <code>6</code> символов (минимум <code>1</code> цифра и <code>1</code> буква)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "first_name",
+            "description": "<p>Имя пользователя</p>"
+          }
+        ]
+      }
+    },
+    "filename": "shower/apps/account/rest/v1/api.py",
+    "groupTitle": "Аутентификация"
+  },
+  {
+    "type": "post",
+    "url": "accounts/resend_activate/",
+    "title": "Письмо активации",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n    \"message\": \"Мы отправили вам письмо на user@example.com\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Response": [
+          {
+            "group": "Response",
+            "type": "String",
+            "optional": false,
+            "field": "message",
+            "description": "<p>Письмо отправлено</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "examples": [
+        {
+          "title": "Error-Response:",
+          "content": "{\n    \"error_code\": 1,\n    \"error_message\": \"30\"\n}",
+          "type": "json"
+        }
+      ],
+      "fields": {
+        "Error Code": [
+          {
+            "group": "Error Code",
+            "optional": false,
+            "field": "1",
+            "description": "<p>Время блокировки в секундах</p>"
+          }
+        ]
+      }
+    },
+    "version": "1.0.0",
+    "header": {
+      "fields": {
+        "Header": [
+          {
+            "group": "Header",
+            "type": "String",
+            "optional": false,
+            "field": "Authorization",
+            "description": "<p>User Bearer Token.</p>"
+          }
+        ]
+      }
+    },
+    "permission": [
+      {
+        "name": "User",
+        "title": "User access rights needed.",
+        "description": "<p>Permission is granted to modify user objects.</p>"
+      }
+    ],
+    "name": "resend_activate",
+    "group": "Аутентификация",
+    "description": "<p>Переотправка письма активации</p>",
+    "filename": "shower/apps/account/rest/v1/api.py",
+    "groupTitle": "Аутентификация"
+  },
+  {
+    "type": "post",
     "url": "accounts/social_auth/:backend/",
     "title": "Авторизация через соц. сети",
     "success": {
